a94b9a66b8dc8e31c158cc8458cf5ce49d93829b
Daniel <dadanner@gmail.com>
2025-03-06 12:48:55 +0100

    Added login screen and Material UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })

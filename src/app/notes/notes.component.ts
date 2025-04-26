4c8ac68c27ad41db24ced2b0b0c4d0375ddf854c
Daniel <dadanner@gmail.com>
2025-04-26 21:09:00 +0200

    Made notes route skeleton

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-notes',
-  imports: [],
+  imports: [MatListModule, MatButtonModule],
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.css'
+  styleUrl: './notes.component.css',
 })
-export class NotesComponent {
-
-}
+export class NotesComponent {}

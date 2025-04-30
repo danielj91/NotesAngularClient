c40f96047a7a7cd573c96c0ae651826a57cc6461
Daniel <dadanner@gmail.com>
2025-05-01 00:43:21 +0200

    Add list of notes to notes route - Get all notes from API - Select a note

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { Note } from '../_models/note';
+import { NotesService } from '../_services/notes.service';
 
 @Component({
   selector: 'app-notes',
@@ -8,4 +10,23 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.css',
 })
-export class NotesComponent {}
+export class NotesComponent implements OnInit {
+  noteList: Note[] = [];
+  selectedNote: Note | null = null;
+
+  constructor(private notesService: NotesService) {}
+
+  ngOnInit(): void {
+    this.getAllNotes();
+  }
+
+  getAllNotes() {
+    this.notesService.getAllNotes().subscribe((notes) => {
+      this.noteList = notes;
+    });
+  }
+
+  onSelectedNote(note: Note) {
+    this.selectedNote = note;
+  }
+}
